a823e1f3efd8d8be17ea8a615996ca22c3e68927
vishubbk <vishubbkup@gmail.com>
2026-06-14 13:23:12 +0530

    brute force done

---
+++ day-11_array/maxsubarray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void maxSumArray(int *arr,int len){
+  int maxSum= INT_MIN;
+  int currSum =0;
+
+  for(int start=0;start<len;start++){
+    for(int end= start;end<len;end++){
+      for(int i=start;i<=end;i++){
+        currSum+=arr[i];
+      }
+      maxSum = max(currSum,maxSum);
+      currSum=0;
+    }
+  }
+  cout <<"Max sum is:"<<maxSum ;
+
+}
+
+int main(){
+    int arr[6]={2,-3,6,-5,4,2};
+    int len = sizeof(arr)/sizeof(int);
+    maxSumArray(arr,len);
+
+  return 0;
+}
