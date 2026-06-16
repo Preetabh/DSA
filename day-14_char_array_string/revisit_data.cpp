cd19f067ee2091a4a13a5f8d7b8c66294d829ec0
vishubbk <vishubbkup@gmail.com>
2026-06-16 12:16:58 +0530

    #

---
+++ day-14_char_array_string/revisit_data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char ch1='a';
+  char ch2='%';
+  char ch3='9';
+  char ch4='a';
+  char ch5='\n';
+  char ch6='a';
+
+  cout << "cht:"<<&ch1<<endl;
+    return 0;
+}
