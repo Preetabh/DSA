bb8a79c86958c1e6a498181037b20c5642b25b0d
vishubbk <vishubbkup@gmail.com>
2026-06-18 13:22:44 +0530

    #

---
+++ day-16_bit/even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num=12;
+  if(!(num &1)){
+    cout <<"This is a even Number";
+
+  }else{
+        cout <<"This is a Odd Number";
+  };
+
+
+    return 0;
+}
