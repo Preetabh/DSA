abcd63fb7186f0d0226dae328ec27b730629bbf4
vishubbk <vishubbkup@gmail.com>
2026-06-23 11:24:05 +0530

    #

---
+++ revision_day/bin_to_dec.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void binToDec(int bin){
+  int pow=1;
+  int n=bin;
+  int dec=0;
+  while(n>0){
+    int lastDeg=bin %10;
+    dec += lastDeg*pow;
+    pow *=2;
+    n= n/10;
+
+  }
+  cout << dec;
+
+}
+
+int main() {
+binToDec(100111);
+    return 0;
+}
