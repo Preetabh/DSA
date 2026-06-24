64dc5deb3c7fc88cdfb49258db61c6865df87e3d
vishubbk <vishubbkup@gmail.com>
2026-06-24 11:37:54 +0530

    #

--- day-13_2D_array/help.cpp
+++ day-13_2D_array/help.cpp
@@ -8,7 +8,10 @@ int main()
       {5, 6, 7, 8},
       {9, 10, 11, 12}
     };
-    cout<< arr[0][2];
+    cout<< arr[2][3] <<endl;
+    cout<< arr[2][2] <<endl;
+    cout<< arr[2][1] <<endl;
+    cout<< arr[2][0] <<endl;
 
   return 0;
 }
