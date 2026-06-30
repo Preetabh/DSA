7d3d05629bae977588ad047ac59bda22c1c283bc
vishubbk <vishubbkup@gmail.com>
2026-06-30 23:59:15 +0530

    #

--- day-20_recursion/print_the_occurrences.cpp
+++ day-20_recursion/print_the_occurrences.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void findIndex(int arr[], int len, int key, int i) {
+    // Base case
+    if (i >= len)
+        return;
+
+    // Work
+    if (arr[i] == key) {
+        cout << i << " ";
+    }
+
+    // Recursive call
+    findIndex(arr, len, key, i + 1);
+}
+
 int main() {
-  int arr[]={3,2,4,5,6,2,7,2,2};
-  int len = sizeof
-    Find_index()
+    int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int key = 2;
+
+    findIndex(arr, len, key, 0);
+
     return 0;
 }
