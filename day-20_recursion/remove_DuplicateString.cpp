aa4fe045f4b86e06cf67204eeab1cfc6492dc804
vishubbk <vishubbkup@gmail.com>
2026-06-30 09:49:05 +0530

    removing Duplicate

---
+++ day-20_recursion/remove_DuplicateString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void removeDuplicate(string str, string ans, int i, int map[26]){
+  if(i==str.size()){
+    cout<<"ans :" << ans<<endl;
+     return;
+  }
+  int mapIdx = (int)(str)[i]-'a';
+  if(map[mapIdx]){
+    removeDuplicate(str,ans,i+1,map);
+  }else{
+    map[mapIdx]=true;
+    removeDuplicate(str,ans+str[i],i+1,map);
+  }
+
+}  
+
+int main() {
+  string str="appnnacollege";
+  string ans="";
+  int map[26]={false};
+
+
+  removeDuplicate(str,ans,0,map);
+
+    return 0;
+}
