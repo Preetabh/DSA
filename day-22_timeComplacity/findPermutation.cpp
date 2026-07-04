2811b31661c437c22f3e62b49ddbd23240c7cf7e
vishubbk <vishubbkup@gmail.com>
2026-07-04 18:41:09 +0530

    findPermutation

---
+++ day-22_timeComplacity/findPermutation.cpp
@@ -0,0 +1,27 @@
+// backTracking approach to find all the permutations of a string
+#include <iostream>
+
+using namespace std;
+
+void findPermutation(string str,string permutation){
+  if(str.length()==0){
+    cout <<permutation<<endl;
+  }
+  else{
+    for(int i=0;i<str.length();i++){
+      char ch=str[i];
+      findPermutation(str.substr(1,str.length()-1),permutation+ch);
+    }
+  }
+}
+
+
+int main() {
+  string str ="abc";
+  int n=str.length();
+  string permutation="";
+  findPermutation(str,permutation);
+
+
+    return 0;
+}
