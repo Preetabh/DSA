11a385ba7a0b62aac52b2e3f49bc99502ec36e94
vishubbk <vishubbkup@gmail.com>
2026-07-07 14:32:34 +0530

    #

--- day-24_backtracking/backtracking_array.cpp
+++ day-24_backtracking/backtracking_array.cpp
@@ -6,20 +6,20 @@ void printArr(int arr[],int n){
 
   }
 }
-void backtrack(int arr[],int n, int i){
+void backtracking(int arr[],int n, int i){
   if(i==n){
     printArr(arr,n);
     return ;
   }
   arr[i]=i+1;
-  backtrack(arr,n,i+1);
+  backtracking(arr,n,i+1);
   arr[i] -=2;
 };
 
 int main() {
   int arr[5]={0};
   int n=5;
-  backtrack(arr,n,0);
+  backtracking(arr,n,0);
   cout << endl;
   printArr(arr,n);
 
